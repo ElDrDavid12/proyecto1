2dac57a180f65c38966c5a679a7ae77255f73cee
Victor David Arrieta Perdomo <v.david.arrieta@gmail.com>
2024-02-18 18:45:03 -0600

    Nueva version

--- AÑO_BISIESTO.py
+++ AÑO_BISIESTO.py
@@ -1,9 +1,5 @@
-modo = input("En un lapso de tiempo los años bisiestos y no bisiestos oh solo un año: ")
-if "lapso de tiempo" or "lapso" in modo:
-    for i in range (0, 2025):
-        if i % 100 == 0 and i % 400 == 1:
-            print(f"El año {i} no es Bisiesto")
-        elif i % 4 == 0:
-            print(f"el año {i} es Bisiesto")
-        elif i % 100 == 0 and i % 400 == 0:
-            print(f"El año {i} puede ser considerado bisiesto.")        
+for i in range(1800, 2025):
+    if (i & 4 == 0 and i % 100 != 0) or (i % 400 == 0):
+        print(f"El año {i} es bisiesto")
+    else:
+        print(f"El año {i} es no bisiesto")    
